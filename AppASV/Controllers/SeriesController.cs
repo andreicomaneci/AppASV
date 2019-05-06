f21ffea3abcadb072951d99e76dfe64c93be1412
a_com <andrei.comaneci@yandex.com>
2019-05-06 22:08:07 +0300

    Create option for genres and series

--- AppASV/Controllers/SeriesController.cs
+++ AppASV/Controllers/SeriesController.cs
@@ -13,13 +13,14 @@ namespace AppASV.Controllers
 		// GET: Series
 		public ActionResult Index()
         {
+			var model = new SeriesViewModel();
 			var series = db.Series;
 			if (TempData.ContainsKey("message"))
 			{
 				ViewBag.message = TempData["message"].ToString();
 			}
 			ViewBag.Series = series;
-			return View();
+			return View(model);
         }
 
 		public ActionResult Show(int id)
@@ -35,32 +36,80 @@ namespace AppASV.Controllers
 			return View(series);
 		}
 
+		[HttpGet]
 		public ActionResult New()
 		{
-			Series series = new Series();
-			return View(series);
+			SeriesViewModel model = new SeriesViewModel();
+			var allGenres = GetAllGenres(); //returns List<Genre>
+			var checkBoxListItems = new List<CheckBoxListItem>();
+			foreach (var genre in allGenres)
+			{
+				checkBoxListItems.Add(new CheckBoxListItem()
+				{
+					ID = 0,
+					Display = genre.Name,
+					IsChecked = false //On the add view, no genres are selected by default
+				});
+			}
+			model.Genres = checkBoxListItems;
+			return View(model);
 		}
 
+		//[HttpPost]
+		//public ActionResult New(Series series)
+		//{
+		//	try
+		//	{
+		//		if (ModelState.IsValid)
+		//		{
+		//			db.Series.Add(series);
+		//			db.SaveChanges();
+		//			TempData["message"] = "The series has been added!";
+		//			return RedirectToAction("Index");
+		//		}
+		//		else
+		//		{
+		//			return View(series);
+		//		}
+		//	}
+		//	catch (Exception e)
+		//	{
+		//		return View(series);
+		//	}
+		//}
+
 		[HttpPost]
-		public ActionResult New(Series series)
+		public ActionResult New(SeriesViewModel model)
 		{
 			try
 			{
 				if (ModelState.IsValid)
 				{
-					db.Series.Add(series);
+					db.Series.Add(model.Series);
+					var selectedGenres = model.Genres.Where(x => x.IsChecked).Select(x => x.Display).ToList();
+					foreach (var genreName in selectedGenres)
+					{
+						SeriesGenre seriesGenre = new SeriesGenre
+						{
+							SeriesId = model.Series.SeriesId,
+							GenreName = genreName
+						};
+						//seriesGenre.Genre = genre;
+						//seriesGenre.Series = model.Series;
+						db.SeriesGenres.Add(seriesGenre);
+					}
 					db.SaveChanges();
 					TempData["message"] = "The series has been added!";
 					return RedirectToAction("Index");
 				}
 				else
 				{
-					return View(series);
+					return View(model);
 				}
 			}
 			catch (Exception e)
 			{
-				return View(series);
+				return View(model);
 			}
 		}
 
@@ -114,6 +163,21 @@ namespace AppASV.Controllers
 			db.SaveChanges();
 			TempData["message"] = "The series has been deleted!";
 			return RedirectToAction("Index");
+		}
+
+		[NonAction]
+		public List<Genre> GetAllGenres()
+		{
+			var selectList = new List<Genre>();
+
+			var genres = from gen in db.Genres select gen;
+
+			foreach (var genre in genres)
+			{
+				selectList.Add(genre);
+			}
+
+			return selectList;
 		}
 	}
 }
---
+++ AppASV/Models/SeriesViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppASV.Models
+{
+	public class SeriesViewModel
+	{
+		public Series Series { get; set; }
+		public List<CheckBoxListItem> Genres { get; set; }
+
+		public SeriesViewModel()
+		{
+			Genres = new List<CheckBoxListItem>();
+		}
+	}
+}
